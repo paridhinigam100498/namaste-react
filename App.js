bb3b9762e66b76e6d54eb76978e05dea3288b0b2
Paridhi Nigam <paridhinigam10@gmail.com>
2023-10-22 21:25:43 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 //getting from node-modules
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "hiiiii from react");
-console.log("heading",heading)//returning object
+// const heading = React.createElement("h1", { id: "heading" }, "hiiiii from react");
+// console.log("heading",heading)//returning object
+
+
+const Title = () => (
+  <h1 className="heading" tabIndex="5">
+    Namaste React Using JSX
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      <h1 className="heading">Namaste React Functional componenet</h1>
+    </div>
+    <div id="container-2"></div>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
